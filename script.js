80fa1c8f1370f2c1d1ab444fd04c35ad143039b9
KeithMurph <keithmurphy94@gmail.com>
2022-04-06 12:35:35 -0700

    fixed box sizes, keyup event not working yet

---
+++ script.js
@@ -0,0 +1,10 @@
+const html_code = document.querySelector('#html_code textarea');
+const css_code = document.querySelector('#css_code textarea');
+const javascript_code = document.querySelector('#javascript_code textarea');
+const result = document.querySelector('#result');
+
+function run () {
+    result.contentDocument.body.innerHTML = `<style>${css_code.value}</style>` + html_code.value;
+}
+
+html_code.onkeyup = () => run();
