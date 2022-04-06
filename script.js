1eecd5d42bbb1cbd4b9b82256d1e04eacb006f96
KeithMurph <keithmurphy94@gmail.com>
2022-04-06 12:58:09 -0700

    Code Editopr is working!!

--- script.js
+++ script.js
@@ -4,7 +4,18 @@ const javascript_code = document.querySelector('.javascript_code textarea');
 const result = document.querySelector('#result');
 
 function run () {
-    result.contentDocument.body.innerHTML = `<style>${css_code.value}</style>` + html_code.value;
+    localStorage.setItem('html_code', html_code.value);
+    localStorage.setItem('css_code', css_code.value);
+    localStorage.setItem('javascript_code', javascript_code.value);
+    result.contentDocument.body.innerHTML = `<style>${localStorage.css_code}</style>` + localStorage.html_code;
+    result.contentWindow.eval(localStorage.javascript_code);
 }
 
 html_code.onkeyup = () => run();
+css_code.onkeyup = () => run();
+javascript_code.onkeyup = () => run();
+
+
+html_code.value = localStorage.html_code;
+css_code.value = localStorage.css_code;
+javascript_code.value = localStorage.javascript_code;
